d14515e528628b4b4c98185d37ca51ac147f1b0b
Berkay Alatas <berkay.3304@gmail.com>
2026-03-05 15:08:25 +0100

    Delete src/features/settings/actions directory

--- src/features/settings/actions/index.ts
+++
@@ -1,8 +0,0 @@
-export { updateProfile } from "@/features/settings/actions/profile";
-export { updatePreference } from "@/features/settings/actions/preferences";
-export { getActivityLogs } from "@/features/settings/actions/activity";
-export {
-  changePassword,
-  revokeSession,
-  logoutOthers,
-} from "@/features/settings/actions/security";
